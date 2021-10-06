bf220eb48e17f4b57e817886f88fbdbf1a7b0d63
Richard <RiEi@hotmail.de>
2021-10-06 20:17:40 +0100

    setup default linting

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './app.css';
 import ReactImage from './react.png';
 
-export default class App extends Component {
-  state = { username: null };
+const App = () => {
+  const [username, setUsername] = useState(null);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('/api/getUsername')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+      .then(user => setUsername(user.username));
+    }, []);
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+      <img src={ReactImage} alt="react" />
+    </div>
+  );
+};
+
+export default App;
